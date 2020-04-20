f69f203e74e05181bef9ab953b8c89a651380b6f
yearing1017 <33566006+yearing1017@users.noreply.github.com>
2020-04-20 10:39:45 +0800

    Create attention.py

---
+++ DAN_ResNet/attention.py
@@ -0,0 +1,74 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class PositionAttentionModule(nn.Module):
+    ''' self-attention '''
+
+    def __init__(self, in_channels):
+        super().__init__()
+        self.query_conv = nn.Conv2d(
+            in_channels, in_channels // 8, kernel_size=1)
+        self.key_conv = nn.Conv2d(in_channels, in_channels // 8, kernel_size=1)
+        self.value_conv = nn.Conv2d(in_channels, in_channels, kernel_size=1)
+        self.gamma = nn.Parameter(torch.zeros(1))
+
+        self.softmax = nn.Softmax(dim=-1)
+
+    def forward(self, x):
+        """
+        inputs :
+            x : feature maps from feature extractor. (N, C, H, W)
+        outputs :
+            feature maps weighted by attention along spatial dimensions
+        """
+
+        N, C, H, W = x.shape
+        query = self.query_conv(x).view(
+            N, -1, H*W).permute(0, 2, 1)  # (N, H*W, C')
+        key = self.key_conv(x).view(N, -1, H*W)  # (N, C', H*W)
+
+        # caluculate correlation
+        energy = torch.bmm(query, key)    # (N, H*W, H*W)
+        # spatial normalize
+        attention = self.softmax(energy)
+
+        value = self.value_conv(x).view(N, -1, H*W)    # (N, C, H*W)
+
+        out = torch.bmm(value, attention.permute(0, 2, 1))
+        out = out.view(N, C, H, W)
+        out = self.gamma*out + x
+        return out
+
+
+class ChannelAttentionModule(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.gamma = nn.Parameter(torch.zeros(1))
+        self.softmax = nn.Softmax(dim=-1)
+
+    def forward(self, x):
+        """
+        inputs :
+            x : feature maps from feature extractor. (N, C, H, W)
+        outputs :
+            feature maps weighted by attention along a channel dimension
+        """
+
+        N, C, H, W = x.shape
+        query = x.view(N, C, -1)    # (N, C, H*W)
+        key = x.view(N, C, -1).permute(0, 2, 1)    # (N, H*W, C)
+
+        # calculate correlation
+        energy = torch.bmm(query, key)    # (N, C, C)
+        energy = torch.max(
+            energy, -1, keepdim=True)[0].expand_as(energy) - energy
+        attention = self.softmax(energy)
+
+        value = x.view(N, C, -1)
+
+        out = torch.bmm(attention, value)
+        out = out.view(N, C, H, W)
+        out = self.gamma*out + x
+        return out
